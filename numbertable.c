c824c05323c918b7521e750430db90df87ee226d
Fazle-Rabbi72 <142036307+Fazle-Rabbi72@users.noreply.github.com>
2023-08-24 12:15:22 -0700

    first commit

---
+++ numbertable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter any number:");
+    scanf("%d",&n);
+    for(i=n;i<=n*10;i=i+n)
+    
+    {
+     printf(" %d\n ",i);
+        
+    }
+    return 0;
+}
